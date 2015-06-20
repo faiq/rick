3ca8d0a40fbccd407c2e65c09119fd9e49d0f5d5
Keeyon Ebrahimi <keeyon2@gmail.com>
2015-06-20 19:14:13 -0400

    Added analyis

--- index.js
+++ index.js
@@ -4,6 +4,44 @@ var express = require("express")
   , bodyParser = require('body-parser')
   , router = express()
 
+var AlchemyAPI = require('./alchemyapi')
+var alchemyapi = new AlchemyAPI()
+
+demo_text = "AlchemyAPI has raised $2 million to extend the capabilities of its deep learning technology that applies artificial intelligence to read and understand web pages, text documents, emails, tweets, and other forms of content. Access Venture Partners led the Series A round, which the company will use to ramp up its sales and marketing, make hires and launch new services."
+
+ 
+function sentiment(text) {
+    alchemyapi.sentiment("text", text, {}, function(response) {
+    console.log("Sentiment: " + response["docSentiment"]["type"])
+    });
+}
+
+function keywords(text) {
+    alchemyapi.keywords("text", text, {}, function(response) {
+        var keywords_json = response["keywords"]
+        var keywords = []
+        Array.prototype.map.call(keywords_json, function(item) {
+            keywords.push(item['text'])
+        })
+        console.log("Keywords: %s", keywords.toString())
+    });
+} 
+
+function concepts(text) {
+    alchemyapi.concepts("text", text, {}, function(response) {
+        var concepts_json = response["concepts"]
+        var concepts = []
+        Array.prototype.map.call(concepts_json, function(item) {
+            concepts.push(item['text'])
+        })
+        console.log("Concepts: %s", concepts.toString())
+    });
+}
+
+sentiment(demo_text)
+keywords(demo_text)
+concepts(demo_text)
+
 router.use(bodyParser.json())       
 router.use(bodyParser.urlencoded({  
   extended: true
@@ -12,5 +50,6 @@ router.use(bodyParser.urlencoded({
 router.use(express.static(path.join(__dirname, '/assets')))
 router.get('/', function (req, res) { 
   res.send('index.html')
+
 })
 
